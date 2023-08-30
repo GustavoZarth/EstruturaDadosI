78fdfb1344ea648ffc01ebd2e6aaccaf55377506
GustavoZP <gustavozarthperotto@gmail.com>
2023-08-30 20:32:52 -0300

    Primeiro Commit

---
+++ Exercicios/exe01/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] numeros = new int[10] { 3, 10, 5, 9, -1, -4, 12, -15, 23, 6 };
+
+Console.Write("Desordenados: ");
+for (int i = 0; i < numeros.Length; i++) {
+    if (i != 9) {
+        Console.Write(numeros[i] + ", ");
+    }
+        else
+    {
+        Console.Write(numeros[i]);
+    }
+}
+
+Array.Sort(numeros);
+
+Console.Write("\nOrdenados: ");
+for (int i = 0; i < numeros.Length; i++) {
+    if (i != 9) {
+        Console.Write(numeros[i] + ", ");
+    }
+    else {
+        Console.Write(numeros[i]);
+    }
+}
